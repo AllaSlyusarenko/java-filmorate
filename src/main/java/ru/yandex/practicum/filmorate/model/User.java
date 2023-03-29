a8a691b85d8d0bbb073e70b2d6a6828a93d76893
Alla <alla.slyu@yandex.ru>
2023-03-29 13:54:59 +0300

    add readme

--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -25,7 +25,5 @@ public class User {
     @NotNull
     private LocalDate birthday;
     private Set<Integer> idFriends = new HashSet<>();
-//    private Set<Integer> offerFromUser = new HashSet<>();
-//    private Set<Integer> offerToUser = new HashSet<>();
 }
 
